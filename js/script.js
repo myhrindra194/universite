23baf3b2911d865faffb2552d0997a8421809c87
Mirindra RANDRIAMBOLAMANJATO <145148402+Myhrindra194@users.noreply.github.com>
2024-03-28 22:17:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@ const navbarClone = document.querySelector("nav").cloneNode(true);
 
 navbarClone.classList.add("fixed-top", "bg-white", "border-bottom");
 
-window.addEventListener("scroll",onscroll = () =>{
-    if(window.scrollY > 0)
-        document.querySelector("body").prepend(navbarClone);
-    else
-        navbarClone.remove()
+window.addEventListener("scroll",onscroll = () => {
+    (window.scrollY > 0) ? document.querySelector("body").prepend(navbarClone): navbarClone.remove();
 })
